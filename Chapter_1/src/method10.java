fd1faf872369260d27d33bb5d32bbc49c2522f89
rani sabale <sablerani97@gmail.com>
2024-10-05 08:29:59 -0700

    added method programs

---
+++ Chapter_1/src/method10.java
@@ -0,0 +1,15 @@
+
+
+public class method10 {
+        public static void main(String[] args){
+            Main15 obj15 = new Main15();
+            System.out.println(obj15.substract(10, 5));
+        }
+
+    }
+
+    class Main15{
+        public int substract(int a, int b){
+            return a-b;
+        }
+    }
