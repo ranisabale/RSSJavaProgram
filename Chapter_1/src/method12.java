068951b6b1c8b547d1e05071776eb9cee5864f2c
rani sabale <sablerani97@gmail.com>
2024-10-14 03:51:06 -0700

    swing and JDBC programns

--- Chapter_1/src/method12.java
+++ Chapter_1/src/method12.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class method12 {
+public class  dmethod12 {
     public static void main(String arg[]){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number: ");
